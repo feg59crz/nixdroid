3b03c53275d1456c5eb78b7674f3bc9f649507c6
feg59crz <fernandogomescruz@gmail.com>
2023-03-09 10:22:50 +0000

    test

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Advanced example of Nix-on-Droid system config with home-manager.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
